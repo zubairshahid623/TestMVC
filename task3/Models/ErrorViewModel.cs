a16a50b9209663d391ad4ec8be5adbf23260bade
Zubair Shahid <Zubair Shahid@DESKTOP-OT2GNFU>
2022-06-27 20:59:34 +0500

    Add project files.

---
+++ task3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace task3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
